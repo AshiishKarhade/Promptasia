e6750051e77a297b8faaf97747f9e3b8714257b8
Ashish Karhade <akarhade5@gmail.com>
2024-06-03 20:32:11 +0530

    Navigation bar setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
+import Feed from '@components/feed';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover and Share
+        </h1>
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        <p className="desc text-center">Promptasia is open-source AI prompting tool for modern world to discover and share creative AI Prompts</p>
+    
+        <Feed />
+    </section>
   )
 }
 
